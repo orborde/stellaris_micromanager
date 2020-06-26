b6277a4f98cccd69782adb0752d41f092a5aa78b
William Ehlhardt <williamehlhardt@gmail.com>
2020-06-25 22:14:28 -0700

    Script to stuff a whole autosave series into a single tarball, which compresses quite nicely

---
+++ save_tree_tar.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+
+import argparse
+import io
+import logging
+import os
+import tarfile
+import sys
+import zipfile
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
+
+parser = argparse.ArgumentParser()
+parser.add_argument("save_directory", type=str)
+args = parser.parse_args()
+
+saves = sorted(fname for fname in os.listdir(args.save_directory))
+
+with os.fdopen(sys.stdout.fileno(), "wb", closefd=False) as stdout:
+    with tarfile.open(mode='w|', fileobj=stdout) as tar:
+        for n, f in enumerate(saves):
+            path = os.path.join(args.save_directory, f)
+            logging.info('processing %d/%d: %s', (n+1), len(saves), path)
+
+            ident, suff = os.path.splitext(f)
+            assert suff == '.sav', suff
+
+            with zipfile.ZipFile(path, 'r') as zf:
+                for cmpt in zf.filelist:
+                    data = zf.read(cmpt)
+
+                    dest = os.path.join(ident, cmpt.filename)
+                    tarinfo = tarfile.TarInfo(dest)
+                    tarinfo.size = len(data)
+                    bytebuf = io.BytesIO(data)
+                    tar.addfile(tarinfo=tarinfo, fileobj=bytebuf)
+
+                    logging.info("%s %s -> %s: %d bytes",
+                        path, cmpt.filename, dest,
+                        len(data))
+
