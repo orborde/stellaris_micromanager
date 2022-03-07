ddcb037b7fddd8ad389c93cc6be5fbd8a1f8c0f6
William Ehlhardt <williamehlhardt@gmail.com>
2022-03-07 11:19:26 -0800

    print out spreads on all resources

--- enumerate_trades.py
+++ enumerate_trades.py
@@ -259,6 +259,16 @@ def profit(bid: Offer, ask: Offer):
     return bid.energy - ask.energy
 
 print(f'{len(all_bids)} bids, {len(all_asks)} asks')
+
+for resource in resources:
+    bids = [o for o in all_bids if o.resource == resource]
+    asks = [o for o in all_asks if o.resource == resource]
+    print(f'{resource}: {len(bids)} bids, {len(asks)} asks')
+    if len(bids) > 0:
+        print('  ', max(bids, key=lambda o: o.price()))
+    if len(asks) > 0:
+        print('  ', min(asks, key=lambda o: o.price()))
+
 matches = [
     (bid, ask)
     for bid,ask in itertools.product(all_bids, all_asks)
